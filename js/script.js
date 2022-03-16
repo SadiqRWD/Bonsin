3e28670d119d747e822a45b7955d566831465195
SadiqRWD <sadiq.rwd.2115@gmail.com>
2022-03-17 00:55:31 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+new VenoBox({
+    selector: '.my-image-links',
+    numeration: true,
+    infinigall: true,
+    share: true,
+    spinner: 'rotating-plane'
+});
+
+$(".designer-slider").slick({
+    slidesToShow : 3,
+    arrows:false,
+    autoplay: true,
+});
+
+new VenoBox({
+    selector: '.my-video-links',
+});
